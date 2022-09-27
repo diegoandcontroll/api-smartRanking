c124e6abf41ab6884dbd23a0bcd226bc06cc7b30
diegoandcontroll <lukasxdp@gmail.com>
2022-09-27 04:08:34 -0300

    feat: add .env to gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://PedroHenrique:senhamongo@cluster0.afxqdz4.mongodb.net/smartranking?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-    ),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
     JogadoresModule,
     ConfigModule.forRoot(),
   ],
